0ea156a059607d299870e2e32cb9244729d67f75
AaravRastogi-TheChef <aarav.rastogi24@gmail.com>
2025-12-23 01:15:51 +0800

    A

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Math',
+    version='0.0.1',
+    description='Deep Library of mathematical operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Aarav Rastogi, Arjun Dev Jha',
+    author_email='aarav.rastogi24@gmail.com, arjundevjha111@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "pytest"
+    ],
+    license='MIT',
+    python_requires='>=3.7',
+
+)
