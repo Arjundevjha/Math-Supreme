493091c6743f28ecfef8edd445488d3e0b78193b
AaravRastogi-TheChef <aarav.rastogi24@gmail.com>
2025-12-23 11:58:31 +0800

    Refactor Compound Interest calculation to a function
    Refactored the compound interest calculation into a function with type hints.

--- Math/Applied_Math/Finance/Compund_intrest.py
+++ Math/Applied_Math/Finance/Compund_intrest.py
@@ -1,10 +1,5 @@
-class CompoundInterestCalculator:
-
-    def main():
-        principal_amount = float(input("Enter the principal amount($): "))
-        interest_rate = float(input("Enter the interest rate(%): "))
-        time = int(input("Enter the time applied: "))
-        compound_frequency = int(input("Enter the number of times interest is compounded per year: "))
+from typing import Union
+def Compound_Interest(principal_amount: Union[int, float], interest_rate: Union[int, float], time:  Union[int, float], compound_frequency: Union[int,float]) -> float:
         compound_interest = principal_amount * (1 + (interest_rate / 100) / compound_frequency) ** (compound_frequency * time)
         print(f"The compound interest is: ${compound_interest:.2f}")
 
