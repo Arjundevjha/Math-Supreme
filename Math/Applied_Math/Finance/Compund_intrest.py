71007ececd0961f93444d8302df5d9d1b7945028
AaravRastogi-TheChef <aarav.rastogi24@gmail.com>
2025-12-23 12:38:27 +0800

    A

--- Math/Applied_Math/Finance/Compund_intrest.py
+++ Math/Applied_Math/Finance/Compund_intrest.py
@@ -1,7 +1,10 @@
 from typing import Union
-def Compound_Interest(principal_amount: Union[int, float], interest_rate: Union[int, float], time:  Union[int, float], compound_frequency: Union[int,float]) -> float:
-        compound_interest = principal_amount * (1 + (interest_rate / 100) / compound_frequency) ** (compound_frequency * time)
-        print(f"The compound interest is: ${compound_interest:.2f}")
 
-if __name__ == "__main__":
-    CompoundInterestCalculator.main()
+def Compound_Interest(principal_amount: Union[int, float], interest_rate: Union[int, float], time:  Union[int, float], compound_frequency: Union[int,float]) -> float:
+
+        if principal_amount < 0 or interest_rate < 0 or time < 0:
+            raise ValueError("Principal amount, interest rate, and time must be non-negative.")
+        
+        compound_interest = principal_amount * (1 + (interest_rate / 100) / compound_frequency) ** (compound_frequency * time)
+
+        return compound_interest
--- Math/Applied_Math/Finance/Simple_Intrest.py
+++ Math/Applied_Math/Finance/Simple_Intrest.py
@@ -1,19 +1,15 @@
-# Simple interest calculator
+#Returns total amount after simple interest
+from typing import Union
 
-class SimpleInterestCalculator:
+def Simple_Interest(principal_amount: Union[int, float], interest_rate: Union[int, float], time: Union[int, float]) -> float:
 
-    def main():
-        principal_amount = float(input("Enter your principal amount ($): "))
-        interest_rate = float(input("Enter the interest rate (%): "))
-        time = float(input("Enter the time applied: "))
         if principal_amount < 0 or interest_rate < 0 or time < 0:
             raise ValueError("Principal amount, interest rate, and time must be non-negative.")
+
         # Calculate simple interest
         interest = (principal_amount * interest_rate * time) / 100
+
         # Calculate total amount after interest
         total_amount = principal_amount + interest
-        print(f"The simple interest is: ${interest:.2f}")
-        print(f"Total amount after {time} at {interest_rate}% simple interest is ${total_amount} ")
+        return total_amount
 
-if __name__ == "__main__":
-    SimpleInterestCalculator.main()
